dd670e5806b1865f85ea20a637e9ec9071d32ca0
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-09-26 12:12:24 +0300

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,15 +5,25 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    to_do = get('https://jsonplaceholder.typicode.com/todos/')
+    to_do_response = to_do.json()
+    users = get('https://jsonplaceholder.typicode.com/users/')
+    users_response = users.json()
+    completed = 0
+    total = 0
+    tasks = []
 
+    for i in users_response:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+    for i in to_do_response:
+        if i.get('userId') == int(argv[1]):
+            total += 1
+            if i.get('completed') is True:
+                completed += 1
+                tasks.append(i.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
+    for i in tasks:
+        print("\t {}".format(i))
