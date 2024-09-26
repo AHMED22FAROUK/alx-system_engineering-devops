3cece7448d80a6978ebd3eaa98302029f182d63d
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-09-26 11:50:29 +0300

    l

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,9 +13,9 @@ def number_of_subscribers(subreddit):
     :param subreddit: The name of the subreddit (str)
     :return: The number of subscribers (int), or 0 if the subreddit is invalid
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
-        "User-Agent": "linux:myredditbot:v1.0.0 (by /u/ahmedfn)"
+        "User-Agent": 'My Usr Agent 1.0'
     }
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
