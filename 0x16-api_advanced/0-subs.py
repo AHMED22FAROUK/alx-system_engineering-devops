f084807e544d30cad51f7c4818be30f77a182497
AHMED22FAROUK <ahmedfaroukcourse1@gmail.com>
2024-09-26 11:39:49 +0300

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "linux:myredditbot:v1.0.0 (by /u/wassola)"
+        "User-Agent": "linux:myredditbot:v1.0.0 (by /u/ahmedfn)"
     }
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
